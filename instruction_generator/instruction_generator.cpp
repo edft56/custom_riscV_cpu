947376340136444bbb540242046df8e187a8af63
edft56 <edft56@hotmail.com>
2022-03-08 03:46:57 +0200

    done with basic instructions

--- instruction_generator/instruction_generator.cpp
+++ instruction_generator/instruction_generator.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 #include <cstdint>
-#include <string>
-#include <vector>
 #include <cmath>
 
-std::vector<std::string> R_instr = {
-		"add","sub", "sll", "slt",
-		"sltu", "xor", "srl", 
-		"sra", "or", "and"
-        };
+
 
 uint32_t generate_alu_rr_instr(){
     uint32_t opcode = 0b0110011;
@@ -27,7 +21,7 @@ uint32_t generate_alu_imm_instr(){
     uint32_t rd_idx = rand() % 31 + 1; //no zeroes
     uint32_t rs1_idx = rand() % 32;
     uint32_t funct3 = rand() % 8;
-    int32_t immediate = ((int32_t)pow(2,11) - 1) - 2*(rand() % (int32_t)pow(2,11));
+    int32_t immediate = (rand() % (int32_t)pow(2,12));
     uint32_t instruction = immediate << 20 | rs1_idx << 15 | funct3 << 12 | rd_idx << 7 | opcode;
     return instruction;
 }
@@ -38,7 +32,7 @@ uint32_t generate_load_instr(){
     uint32_t rs1_idx = rand() % 32;
     uint32_t funct3_values[5] = {0,1,2,4,5};
     uint32_t funct3 = funct3_values[rand() % 5];
-    int32_t immediate = ((int32_t)pow(2,11) - 1) - 2*(rand() % (int32_t)pow(2,11));
+    int32_t immediate = (rand() % (int32_t)pow(2,12));
     uint32_t instruction = immediate << 20 | rs1_idx << 15 | funct3 << 12 | rd_idx << 7 | opcode;
     return instruction;
 }
@@ -48,21 +42,69 @@ uint32_t generate_jalr_instr(){
     uint32_t rd_idx = rand() % 31 + 1; //no zeroes
     uint32_t rs1_idx = rand() % 32;
     uint32_t funct3 = 0;
-    int32_t immediate = ((int32_t)pow(2,11) - 1) - 2*(rand() % (int32_t)pow(2,11));
+    int32_t immediate = (rand() % (int32_t)pow(2,12));
     uint32_t instruction = immediate << 20 | rs1_idx << 15 | funct3 << 12 | rd_idx << 7 | opcode;
     return instruction;
 }
 
 uint32_t generate_store_instr(){
     uint32_t opcode = 0b0100011;
-    uint32_t rs2_idx = rand() % 32; //no zeroes
+    uint32_t rs2_idx = rand() % 32;
     uint32_t rs1_idx = rand() % 32;
     uint32_t funct3 = rand() % 3;
-    int32_t immediate = ((int32_t)pow(2,11) - 1) - 2*(rand() % (int32_t)pow(2,11));
+    int32_t immediate = (rand() % (int32_t)pow(2,12));
     int32_t imm_field1 = immediate & 0x00000FE0;
     int32_t imm_field2 = immediate & 0x0000001F;
     uint32_t instruction = imm_field1 << 20 | rs1_idx << 15 | funct3 << 12 | imm_field2 << 7 | opcode;
     return instruction;
 }
+
+uint32_t generate_lui_instr(){
+    uint32_t opcode = 0b0110111;
+    uint32_t rd_idx = rand() % 31 + 1; //no zeroes
+    int32_t immediate = rand() % (int32_t)pow(2,20);
+
+    uint32_t instruction = immediate << 7 | opcode;
+    return instruction;
+}
+
+uint32_t generate_auipc_instr(){
+    uint32_t opcode = 0b0010111;
+    uint32_t rd_idx = rand() % 31 + 1; //no zeroes
+    int32_t immediate = rand() % (int32_t)pow(2,20);
+
+    uint32_t instruction = immediate << 7 | opcode;
+    return instruction;
+}
+
+uint32_t generate_branch_instr(){
+    uint32_t opcode = 0b1100011;
+    uint32_t rs2_idx = rand() % 32;
+    uint32_t rs1_idx = rand() % 32;
+    uint32_t funct3_values[6] = {0,1,4,5,6,7};
+    uint32_t funct3 = funct3_values[rand() % 6];
+    int32_t immediate = rand() % (int32_t)pow(2,12);
+    int32_t imm_field1 = immediate & 0x00000800;
+    int32_t imm_field2 = immediate & 0x000003F0;
+    int32_t imm_field3 = immediate & 0x0000000F;
+    int32_t imm_field4 = immediate & 0x00000400;
+    int32_t final_immediate = imm_field1 << 31 | imm_field2 << 25 | imm_field3 << 8 | imm_field4 << 7;
+    uint32_t instruction = final_immediate | rs2_idx << 20 | rs1_idx << 15 | funct3 << 12 | opcode;
+    return instruction;
+}
+
+uint32_t generate_jal_instr(){
+    uint32_t opcode = 0b1101111;
+    uint32_t rd_idx = rand() % 31 + 1; //no zeroes
+    int32_t immediate = rand() % (int32_t)pow(2,20);
+    int32_t imm_field1 = immediate & 0x00080000;
+    int32_t imm_field2 = immediate & 0x000003FF;
+    int32_t imm_field3 = immediate & 0x00000400;
+    int32_t imm_field4 = immediate & 0x0007F800;
+    int32_t final_immediate = imm_field1 << 31 | imm_field2 << 21 | imm_field3 << 20 | imm_field4 << 12;
+    uint32_t instruction = final_immediate | rd_idx << 7 | opcode;
+    return instruction;
+}
+
 
 
